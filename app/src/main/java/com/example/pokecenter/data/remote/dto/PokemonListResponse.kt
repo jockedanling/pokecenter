dae2280b4c83f5ec21cd8370f14b847b48c1a780
Joakim Danling <j.danling@gmail.com>
2026-09-03 11:24:01 +0200

    add dto response

---
+++ app/src/main/java/com/example/pokecenter/data/remote/dto/PokemonListResponse.kt
@@ -0,0 +1,16 @@
+package com.example.pokecenter.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PokemonListResponse (
+    val count: Int,
+            val next: String?,
+                    val previous: String?,
+                            val results: List<PokemonListItemDto>
+    )
+@Serializable
+data class PokemonListItemDto(
+    val name: String,
+    val url: String
+)
