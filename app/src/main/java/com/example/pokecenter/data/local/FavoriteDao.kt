38225ada266e0584f07f7e326e261177ed9fff9d
Joakim Danling <j.danling@gmail.com>
2026-09-07 15:54:45 +0200

    fix room bugs and add pokemonDatabase file

--- app/src/main/java/com/example/pokecenter/data/local/FavoriteDao.kt
+++ app/src/main/java/com/example/pokecenter/data/local/FavoriteDao.kt
@@ -1,7 +1,6 @@
 package com.example.pokecenter.data.local
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.OnConflictStrategy
